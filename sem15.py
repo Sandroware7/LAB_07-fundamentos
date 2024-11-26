3c311d450cefdd2aceabab8c0481f6a998c2230c
Sandro Rodríguez <sandro.rodriguez.b@tecsup.edu.pe>
2024-11-26 10:15:31 -0500

    sem 15

---
+++ sem15.py
@@ -0,0 +1,16 @@
+class dog:
+    def __ini__(self, nameP, ageP):
+        self.name = nameP
+        self.age = ageP
+
+    def bark(self):
+        print(f"{self.name} bark")
+
+    def eat(self):
+        print(f"{self.name}eat")
+
+    def sleep(self):
+        print(f"{self.name}" sleep)
+
+        
+        
